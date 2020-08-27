138d9d1d9bcd8c4bb291218bc3023ac644c89f9e
Jagannath Mishra <42279500+Jagannath-Mishra@users.noreply.github.com>
2020-08-27 07:44:52 +0530

    21. Updating Widget Data (Or Using StatelessWidget Incorrectly)

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,15 @@ void main() => runApp(MyApp());
 
 //Format flutter Code: CTRL + ALT + L
 class MyApp extends StatelessWidget {
-  var questions = ["what's your fav color", "What's your fab sports"];
+  var questionIndex = 0;
+  var questions = ["what's your fav color ?", "What's your fab sports ?", "What's your first job?"];
   void answarClicked(){
-    print('answar choosen - 3');
+    if(questionIndex < 3){
+      questionIndex++;
+      print("Selected question " + questionIndex.toString());
+    }else {
+      print('We are out of questions!');
+    }
   }
   @override
   Widget build(BuildContext context) {
@@ -21,16 +27,14 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The Questions!'),
+            Text(questions.elementAt(questionIndex)),
             RaisedButton(
               child:Text('Answar-1'),
-              onPressed: () => print("Answar 1 chosen!"),
+              onPressed: answarClicked,
             ),
             RaisedButton(
               child:Text('Answar-2'),
-              onPressed: () => {
-                print('Answar 2 chosen!')
-              },
+              onPressed: answarClicked,
             ),
             RaisedButton(
               child: Text('Answar-3'),

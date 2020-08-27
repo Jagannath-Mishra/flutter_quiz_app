47fb7a05410d0a00eb0d60b4ee76edcd34b50414
Jagannath Mishra <42279500+Jagannath-Mishra@users.noreply.github.com>
2020-08-27 10:35:58 +0530

    23. Updating Correctly with Stateful Widgets Converted stateless to statefull.

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,33 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 //Format flutter Code: CTRL + ALT + L
-class MyApp extends StatelessWidget {
-  var questionIndex = 0;
-  var questions = ["what's your fav color ?", "What's your fab sports ?", "What's your first job?"];
-  void answarClicked(){
-    if(questionIndex < 3){
-      questionIndex++;
-      print("Selected question " + questionIndex.toString());
-    }else {
-      print('We are out of questions!');
-    }
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyStateApp();
   }
+}
+
+class MyStateApp extends State<MyApp> {
+  var questionIndex = 0;
+  var questions = [
+    "what's your fav color ?",
+    "What's your fab sports ?",
+    "What's your first job?"
+  ];
+
+  void answarClicked() {
+    setState(() {
+      if (questionIndex < 3) {
+        questionIndex++;
+        print("Selected question " + questionIndex.toString());
+      } else {
+        print('We are out of questions!');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,11 +45,11 @@ class MyApp extends StatelessWidget {
           children: [
             Text(questions.elementAt(questionIndex)),
             RaisedButton(
-              child:Text('Answar-1'),
+              child: Text('Answar-1'),
               onPressed: answarClicked,
             ),
             RaisedButton(
-              child:Text('Answar-2'),
+              child: Text('Answar-2'),
               onPressed: answarClicked,
             ),
             RaisedButton(
